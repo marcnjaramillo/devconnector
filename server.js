6d12a962eeb26b1090c4e11ea952921c91af97ca
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-05-11 11:00:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-  extended: false
-}));
+app.use(
+  express.json({
+    extended: false
+  })
+);
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +19,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
